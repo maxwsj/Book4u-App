e35b4efa36251ac64b6920d82fd532aa15217874
Max William <83377057+maxwsj@users.noreply.github.com>
2022-06-06 00:22:57 -0300

    Update Config.js

--- starting-project/util/Config.js
+++ starting-project/util/Config.js
@@ -1,6 +1,6 @@
 const Config = {
    //pra teste no lugar do localhost voce coloca seu ip local
-   API_URL: 'http://192.168.0.108:4000/',
+   API_URL: 'http://:4000/',
    TIMEOUT_REQUEST: 5000,
    HEADER_REQUEST: {
       post: {
